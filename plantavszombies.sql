7c831dcf5c600dedc5c52f50f154c69851313197
Ignacioe <mypromega@hotmail.com>
2018-08-30 18:50:47 -0300

    Cambio base de datos mediante Visual Studio

--- plantavszombies.sql
+++ plantavszombies.sql
@@ -30,25 +30,29 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cartas` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `clase` varchar(20) NOT NULL,
-  `tipo` varchar(20) NOT NULL,
-  `coste` int(10) NOT NULL,
-  `ataque` int(10) NOT NULL,
-  `salud` int(10) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
-  `rareza` varchar(20) NOT NULL,
-  `expamsion` varchar(20) NOT NULL
+  `Nombre` varchar(30) NOT NULL,
+  `Clase` varchar(20) NOT NULL,
+  `Tipo` varchar(20) NOT NULL,
+  `Coste` int(10) NOT NULL,
+  `Ataque` int(10) NOT NULL,
+  `Salud` int(10) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Rareza` varchar(20) NOT NULL,
+  `Tribu` varchar(20) NOT NULL,
+  `Tribu2` varchar(20) NOT NULL,
+  `Tribu3` varchar(20) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `cartas`
 --
 
-INSERT INTO `cartas` (`id`, `nombre`, `clase`, `tipo`, `coste`, `ataque`, `salud`, `descripcion`, `rareza`, `expamsion`) VALUES
-(0, 'lanzaguisantes', 'gigante', 'planta', 1, 1, 1, '', 'comun', ''),
-(1, 'lanzaguisantes', 'gigante', 'planta', 1, 1, 1, '-', 'comun', '-'),
-(2, 'Platanosaurio Rex', 'gigante', 'planta', 4, 3, 3, 'Doble impacto. Dinorrugido: otiene +1/+1', 'Legendaria', 'Prémium');
+INSERT INTO `cartas` (`id`, `Nombre`, `Clase`, `Tipo`, `Coste`, `Ataque`, `Salud`, `Descripcion`, `Rareza`, `Expansion`, `Tribu`, `Tribu2`, `Tribu3`) VALUES
+(0, 'Ajo', 'Guardián', 'Planta', 1, 1, 5, 'Cuando un Zombi le inflige daño, mueve a ese Zombi a la izquierda. Si es un Vampidito, lo destruye.', 'Evento', '', '', '', ''),
+(1, 'Nuezmeolvides', 'Guardián', 'Planta', 1, 2, 1, 'Los Trucos Zombis cuestan 1 mas.', 'Evento', '', '', '', ''),
+(2, 'Platanosaurio Rex', 'gigante', 'planta', 4, 3, 3, 'Doble impacto. Dinorrugido: obtiene +1/+1', 'Legendaria', 'Prémium', '', '', '');
+
 
 -- --------------------------------------------------------
 
