434f845991615e7b654ba82b051a41a8145ed339
Giuliano Regolo <gripdesexto6e@gmail.com>
2018-08-29 22:52:40 -0300

    lo tengo hasta ahora sin la pagina priniciapl, esa esta en el drive

---
+++ formulariog/base-de-datos.php
@@ -0,0 +1,20 @@
+<?php
+
+mb_internal_encoding("UTF-8");
+
+$Coneccion = mysqli_connect("localhost","root","","plantavszombies") or die("no se encuentra el servidor");
+
+$Nombre = $_POST['Nombre'];
+$apellido = $_POST['apellido'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$sexo = $_POST['sexo'];
+$continente = $_POST['continente'];
+
+mysqli_set_charset($conexion, 'utf8');
+
+$ingresar = "INSERT INTO usuarios (Nombre, apellido,email,password,sexo,continente) VALUES ('$Nombre','$apellido','$email','$password','$sexo','$continente')";
+
+$query = mysqli_query($Coneccion,$ingresar);
+
+?>
